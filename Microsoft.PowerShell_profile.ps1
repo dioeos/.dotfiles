bd21e44ae33c4dd8ab7c3b7b21d9a00e5475ea6d
Ryan Dioneda <er.dioneda@gmail.com>
2025-04-10 16:35:33 -0400

    Update - profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,41 @@
+#komorebic functions
+function kstart {
+    param (
+        [string]$arg
+    )
+
+    if ($arg -eq "bar") {
+        komorebic start --whkd --bar
+    } else {
+        komorebic start --whkd
+    }
+}
+
+function kstop {
+    komorebic stop
+}
+
+#navigation functions
+function j {Set-Location ..}
+
+function 2j {Set-Location ..\..}
+
+function 3j {Set-Location ..\..\..}
+
+function 4j {Set-Location ..\..\..\..}
+
+#general
+
+function rm {Remove-Item $args}
+function mv {Move-Item $args}
+function touch {New-Item $args}
+
+#path configurations
+$ahkPath = "C:\Program Files\AutoHotkey\v2\AutoHotkey.exe"
+$change_layout_ahk = "C:\Users\eryan\.dotfiles\AutoHotkey\change_layout.ahk"
+
+
+#startup configurations
+Start-Process -FilePath $ahkPath -ArgumentList $change_layout_ahk
+
+#alias 
