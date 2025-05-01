8596fa3c97844c464a911f7925179c30f87b317b
Ryan Dioneda <er.dioneda@gmail.com>
2025-05-01 18:12:06 -0400

    Added wezterm config

---
+++ Wezterm/.wezterm.lua
@@ -0,0 +1,100 @@
+local wezterm  = require 'wezterm'
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.wsl_domains = {
+    {
+        name = 'WSL:Ubuntu',
+        distribution = 'Ubuntu',
+        default_cwd = '~',
+    },
+}
+config.default_domain = 'WSL:Ubuntu'
+config.font = wezterm.font 'JetBrains Mono'
+
+-- FONT & TEXTS
+config.font_size = 13.0
+
+-- WINDOW DECORATIONS
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.75
+config.window_decorations = "RESIZE"
+config.window_padding ={
+    left = 0,
+    right = 0,
+    top = 2.5,
+    bottom = 2.5,
+}
+config.adjust_window_size_when_changing_font_size = false
+
+config.keys = {
+    {
+        key = 'X',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+    {
+        key = '|',
+        mods = 'CTRL|SHIFT',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = 'H',
+        mods = 'CTRL|SHIFT',
+        action = act.AdjustPaneSize { 'Left', 5 },
+    },
+    {
+        key = 'L',
+        mods = 'CTRL|SHIFT',
+        action = act.AdjustPaneSize { 'Right', 5 },
+    },
+    {
+        key = 'J',
+        mods = 'CTRL|SHIFT',
+        action = act.AdjustPaneSize { 'Down', 5 },
+    },
+    {
+        key = 'K',
+        mods = 'CTRL|SHIFT',
+        action = act.AdjustPaneSize { 'Up', 5 },
+    },
+    {
+        key = '<',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Left'
+    },
+    {
+        key = '>',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Right'
+    },
+    {
+        key = 'UpArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Up'
+    },
+    {
+        key = 'DownArrow',
+        mods = 'CTRL|SHIFT',
+        action = act.ActivatePaneDirection 'Down'
+    },
+
+    {
+        key = "p",
+        mods = "CTRL|SHIFT",
+        action = act.ActivateTabRelative(-1),
+    },
+    {
+        key = "n",
+        mods = "CTRL|SHIFT",
+        action = act.ActivateTabRelative(1),
+    },
+    {
+      key = "0",
+      mods = "CTRL",
+      action = wezterm.action.ResetFontSize,
+    },
+}
+
+
+return config
