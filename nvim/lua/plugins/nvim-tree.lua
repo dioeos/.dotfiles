84e142ea76fe60b9f16a3864acf6809d6c6f700d
Ryan Dioneda <er.dioneda@gmail.com>
2025-05-08 10:52:59 -0400

    Configured marks for nvim

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,33 +1,33 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    lazy = false,
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-    },
+	"nvim-tree/nvim-tree.lua",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 
-    config = function()
-        -- FILE TREE --
-        -- disable netrw at the very start of your init.lua
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+	config = function()
+		-- FILE TREE --
+		-- disable netrw at the very start of your init.lua
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-        -- optionally enable 24-bit colour
-        vim.opt.termguicolors = true
+		-- optionally enable 24-bit colour
+		vim.opt.termguicolors = true
 
-        -- OR setup with some options
-        require("nvim-tree").setup({
-          sort = {
-            sorter = "case_sensitive",
-          },
-          view = {
-            width = 30,
-          },
-          renderer = {
-            group_empty = true,
-          },
-          filters = {
-            dotfiles = true,
-          },
-        })
-    end,
+		-- OR setup with some options
+		require("nvim-tree").setup({
+			sort = {
+				sorter = "case_sensitive",
+			},
+			view = {
+				width = 30,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = false,
+			},
+		})
+	end,
 }
