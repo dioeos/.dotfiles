a5bd1d4941fce03cf70bc20eeba29436b44ab3ad
Ryan Dioneda <er.dioneda@gmail.com>
2025-05-19 01:42:20 -0400

    config update

--- Scripts/yasb_komorebi_startup.ps1
+++ Scripts/yasb_komorebi_startup.ps1
@@ -14,6 +14,6 @@ Start-Sleep -Seconds 1
 
 Start-Process "C:\Program Files\yasb\yasb.exe" -ArgumentList '--config', 'C:/Users/eryan/.dotfiles/yasb/yasb.toml'
 
-Start-Sleep -Seconds 100
+Start-Sleep -Seconds 10
 
 Start-Process "C:\Users\eryan\AppData\Local\Microsoft\WindowsApps\Spotify.exe"
