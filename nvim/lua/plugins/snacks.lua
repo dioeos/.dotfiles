5c2a771efcc98c1c0135cf4bf0b1049d7ef71c06
Ryan Dioneda <er.dioneda@gmail.com>
2026-02-27 15:18:25 -0500

    UFO nvim

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,24 +1,70 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
 
-    config = function()
-        require("snacks").setup({
-            bigfile = { enabled = true },
-            dashboard = { enabled = true },
-            explorer = { enabled = false },
-            indent = { enabled = false },
-            input = { enabled = true },
-            picker = { enabled = true },
-            notifier = { enabled = true },
-            quickfile = { enabled = true },
-            scope = { enabled = true },
-            scroll = { enabled = false },
-            statuscolumn = { enabled = true },
-            words = { enabled = true },
-        })
-    end,
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = {
+			preset = {
+				pick = nil,
+				---@type snacks.dashboard.Item[]
+				keys = {
+					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+					{
+						icon = " ",
+						key = "g",
+						desc = "Find Text",
+						action = ":lua Snacks.dashboard.pick('live_grep')",
+					},
+					{
+						icon = " ",
+						key = "r",
+						desc = "Recent Files",
+						action = ":lua Snacks.dashboard.pick('oldfiles')",
+					},
+					{
+						icon = " ",
+						key = "c",
+						desc = "Config",
+						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+					},
+					{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+					{
+						icon = "󰒲 ",
+						key = "l",
+						desc = "Lazy",
+						action = ":Lazy",
+						enabled = package.loaded.lazy ~= nil,
+					},
+					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+				},
+				header = [[
+
+        ]],
+			},
+			sections = {
+				{ section = "header" },
+				{
+					section = "keys",
+					indent = 1,
+					padding = 1,
+				},
+				{ section = "recent_files", icon = " ", title = "Recent Files", indent = 3, padding = 2 },
+				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{ section = "startup" },
+			},
+		},
+		explorer = { enabled = false },
+		indent = { enabled = false },
+		input = { enabled = true },
+		picker = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+	},
 }
-
-
